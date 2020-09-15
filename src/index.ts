e1790c4fd6601fa780f08f56d7948b817bd5d1d0
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-09-14 18:22:25 -0700

    Addeded esLint to the project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//Intentionally awful code for showing how a linter works
+
+function HelloWorld(message:string)
+{
+    console.log("Hello World");
+    let updatedMessage;
+
+    return 0
+}
+
+HelloWorld("Goodbye cruel world");

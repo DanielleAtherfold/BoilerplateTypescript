be2924c3f18f724803b44d88a0fc39a962c6693f
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-09-14 19:26:07 -0700

    Configured scripts for fix and nofix variants.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-
 function HelloWorld(message: string) {
-    console.log("Hello World");
-    let updatedMessage = { f: 1, b: 2 };
+    console.log("Hello World!");
+    const updatedMessage = { f: 1, b: 2 };
 
     return 0;
 }
